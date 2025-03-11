c8e72fa1756f51e44845d3847709c522aa0dc4ad
Artaius <36020251+xartaiusx@users.noreply.github.com>
2025-03-11 00:43:16 -0700

    Update Fighter.cs

--- Assets/Scripts/Core/Fighter.cs
+++ Assets/Scripts/Core/Fighter.cs
@@ -1,1 +1,39 @@
+public class Fighter : BaseCharacter
+{
+    private const int HealthMultiplier = 10;
 
+    public override void InitializeStats()
+    {
+        strength = 10;
+        stamina = 8;
+        intelligence = 3;
+
+        maxHealth = stamina * HealthMultiplier;
+        currentHealth = maxHealth;
+    }
+
+    public override void LevelUp()
+    {
+        level++;
+        IncreaseStats();
+        UpdateHealth();
+    }
+
+    private void IncreaseStats()
+    {
+        strength += 3;
+        stamina += 2;
+        intelligence += 1;
+    }
+
+    private void UpdateHealth()
+    {
+        maxHealth = stamina * HealthMultiplier;
+        
+        // Only reset current health if full healing is desired
+        if (currentHealth == maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+    }
+}
